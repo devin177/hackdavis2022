d74d25dda707634a8bee3e2009fd30d4dcdb71b8
Jimmy-Trinh <trinhmjimmy@gmail.com>
2022-04-16 15:10:58 -0700

    added mongodb database

--- server.js
+++ server.js
@@ -3,8 +3,27 @@ const app = express()
 const port = 3000
 const QRCode = require('qrcode')
 const bp = require('body-parser')
+const { MongoClient } = require("mongodb");
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
+
+// Connection URI
+const uri = "mongodb+srv://admin:rQbMxlDmogFNavik@aggierewards.5psgr.mongodb.net/aggieSpirit?retryWrites=true&w=majority";
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Generate a QR code based on the verify url
 app.post('/generate', (req, res) => {
